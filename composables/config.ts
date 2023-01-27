a1294c5f09a19f52f167b9a72c6dda929e66f038
jacob zlogar <jacob@vaultinnovation.com>
2023-01-27 17:31:00 -0500

    ok

--- composables/config.ts
+++ composables/config.ts
@@ -1,6 +1,6 @@
 export class Config {
   url = (): string => {
-    return "af35cf077978d416c8ce5a31bf7cdd99-1754053019.us-east-1.elb.amazonaws.com"
+    return "a6936de316b504756b04e89985915046-1065067211.us-east-1.elb.amazonaws.com"
   };
   nest_api_url = (): string => {
     return `${process.env.NEXT_PUBLIC_NEST_PORT}`;
